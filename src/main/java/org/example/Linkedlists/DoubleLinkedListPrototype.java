32c753c883ec0823a3634164514c8e0c4a1c28bf
Mourya1992 <themourya.v@yahoo.com>
2023-04-02 11:26:57 +0530

    Added Linked List prototype class and double linked list delete and addLast methods added

--- src/main/java/org/example/Linkedlists/DoubleLinkedListPrototype.java
+++ src/main/java/org/example/Linkedlists/DoubleLinkedListPrototype.java
@@ -3,38 +3,97 @@ package org.example.Linkedlists;
 public class DoubleLinkedListPrototype {
 
     DoubleLinkedListNode head;
+
     public static void main(String[] args) {
         DoubleLinkedListPrototype dl = new DoubleLinkedListPrototype();
-        dl.addFirst("one");
-        dl.addFirst("two");
+        dl.addLast("one");
+        dl.addLast("two");
+        dl.addLast("three");
+        dl.addLast("four");
+        dl.addFirst("zero");
+
+        dl.printDoubleLinkedList();
+        dl.deleteFirst();
+        dl.deleteFirst();
+        dl.deletelast();
+        dl.deletelast();
+        dl.deleteFirst();
         dl.printDoubleLinkedList();
 
     }
 
-    public void printDoubleLinkedList(){
-        if(head == null){
+    public void printDoubleLinkedList() {
+        if (head == null) {
             System.out.print("List is empty");
             return;
         }
         DoubleLinkedListNode currentNode = head;
-        while(currentNode!=null){
-            System.out.print(currentNode.data+"->");
-            currentNode=currentNode.nextNode;
+        while (currentNode != null) {
+            System.out.print(currentNode.data + "->");
+            currentNode = currentNode.nextNode;
         }
+        System.out.println("NULL");
     }
 
-
-    public void addFirst(String data){
+    public void addFirst(String data) {
         DoubleLinkedListNode newNode = new DoubleLinkedListNode(data);
-        if(head == null){
-            head=newNode;
+        if (head == null) {
+            head = newNode;
             return;
         }
-        newNode.previousNode=null;
-        newNode.nextNode=head;
-        head.previousNode=newNode;
-        head=newNode;
+        newNode.previousNode = null;
+        newNode.nextNode = head;
+        head.previousNode = newNode;
+        head = newNode;
 
     }
+
+    public void addLast(String data) {
+        DoubleLinkedListNode newNode = new DoubleLinkedListNode(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        DoubleLinkedListNode currentNode = head;
+        while (currentNode.nextNode != null) {
+            currentNode = currentNode.nextNode;
+
+        }
+        newNode.nextNode = null;
+        currentNode.nextNode = newNode;
+        newNode.previousNode = currentNode;
+
+
+    }
+
+    public void deleteFirst() {
+        if (head == null) {
+            System.out.println("the List is empty:");
+        }
+        if(head.nextNode==null){
+            head=null;
+            return;
+        }
+        head.nextNode.previousNode = null;
+        head = head.nextNode;
+    }
+
+    public void deletelast() {
+        if (head == null) {
+            System.out.println("the List is empty:");
+        }
+        if (head.nextNode == null) {
+            head = null;
+            return;
+        }
+        DoubleLinkedListNode currentNode = head;
+        DoubleLinkedListNode previousCurrentNode = null;
+        while (currentNode.nextNode != null) {
+            previousCurrentNode = currentNode;
+            currentNode = currentNode.nextNode;
+        }
+        previousCurrentNode.nextNode = null;
+    }
+
 
 }
