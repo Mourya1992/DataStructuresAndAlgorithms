62c9a20f9574c53d2081f9a9464c9500e70d1316
Mourya1992 <themourya.v@yahoo.com>
2023-09-16 18:02:53 +0530

    Graphs  Data structure-Dijkestra Algorithms

--- src/main/java/org/example/Graphs/Edge.java
+++ src/main/java/org/example/Graphs/Edge.java
@@ -4,9 +4,17 @@ public class Edge {
     private int source;
     private int destination;
 
+    private  int weight;
+
     public Edge(int source, int destination) {
         this.source = source;
         this.destination = destination;
+    }
+
+    public Edge(int source, int destination,int weight) {
+        this.source = source;
+        this.destination = destination;
+        this.weight=weight;
     }
 
     public int getSource() {
@@ -20,5 +28,14 @@ public class Edge {
 
     public int getDestination() {
         return destination;
+    }
+
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
     }
 }
--- src/main/java/org/example/Graphs/GraphsDS.java
+++ src/main/java/org/example/Graphs/GraphsDS.java
@@ -1,11 +1,30 @@
 package org.example.Graphs;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class GraphsDS {
+
+
+    public static List<ArrayList<Edge>> createWeightedGraph(){
+        int vertexCount = 6;
+        List<ArrayList<Edge>> graph = new ArrayList<>();
+
+        for (int i = 0; i < vertexCount; i++) {
+            graph.add(new ArrayList<>());
+        }
+        graph.get(0).add(new Edge(0, 1,2));
+        graph.get(0).add(new Edge(0, 2,4));
+        graph.get(1).add(new Edge(1, 2,1));
+        graph.get(1).add(new Edge(1, 3,7));
+        graph.get(2).add(new Edge(2, 4,3));
+        graph.get(3).add(new Edge(3, 5,1));
+        graph.get(4).add(new Edge(4, 3,2));
+        graph.get(4).add(new Edge(4, 5,5));
+
+        return graph;
+
+    }
+
 
 
     public static List<ArrayList<Edge>> createGraph() {
@@ -43,7 +62,7 @@ public class GraphsDS {
 
 
     public List<ArrayList<Edge>>
-    getSmallGraph(int vertexCount){
+    getSmallGraph(int vertexCount) {
         List<ArrayList<Edge>> graph = new ArrayList<>();
 
         for (int i = 0; i <= vertexCount; i++) {
@@ -59,8 +78,8 @@ public class GraphsDS {
 
         return graph;
     }
-    public static void printTheNeighbour(List<ArrayList<Edge>> graph) {
-        int vertexCount = 3;
+
+    public static void printTheNeighbour(List<ArrayList<Edge>> graph, int vertexCount) {
         int i = 0;
         while (i < vertexCount) {
             int finalI = i;
@@ -69,11 +88,36 @@ public class GraphsDS {
         }
     }
 
+    public static List<ArrayList<Edge>> smallCyclicGraph() {
+        List<ArrayList<Edge>> graphDs = new ArrayList<>();
+        ArrayList<Edge> vertex1 = new ArrayList<Edge>();
+        vertex1.add(new Edge(0, 1));
+        vertex1.add(new Edge(0, 2));
+        ArrayList<Edge> vertex2 = new ArrayList<Edge>();
+        vertex2.add(new Edge(1, 0));
+        vertex2.add(new Edge(1, 2));
+        ArrayList<Edge> vertex3 = new ArrayList<Edge>();
+        vertex3.add(new Edge(2, 0));
+        vertex3.add(new Edge(2, 1));
+        graphDs.add(vertex1);
+        graphDs.add(vertex2);
+        graphDs.add(vertex3);
+        return graphDs;
+    }
+
     public static void main(String[] args) {
         GraphsDS gpDs = new GraphsDS();
-        List<ArrayList<Edge>> graphDs = createGraph();
-      //  printTheNeighbour(graphDs);
-        boolean visitedList[] = new boolean[graphDs.size()];
+
+        List<ArrayList<Edge>> graphDs = createWeightedGraph();
+        boolean[] visited = new boolean[graphDs.size()];
+        int[] distanceMatrix = new int[graphDs.size()];
+        System.out.println("distance matrix size:::"+distanceMatrix.length);
+        dijestrasAlgorithmTofindtheShortestPath(graphDs,visited,distanceMatrix,5);
+
+        //bfsTest(graphDs);
+
+        //  printTheNeighbour(graphDs);
+      /* ;
         for (int i = 0; i < visitedList.length; i++) {
             if (visitedList[i] == false) {
                 breadthFirstSearchAlgorithm(graphDs, visitedList, i);
@@ -85,14 +129,101 @@ public class GraphsDS {
 
         System.out.println("Find all the paths of between source and destination");
         vis = new boolean[graphDs.size()];
-        gpDs.printAllPathsUsingDFS(graphDs,vis,0, 2,"0");
+        gpDs.printAllPathsUsingDFS(graphDs,vis,0, 2,"0");*/
+
+    }
+
+    static class NodeData implements Comparable<NodeData> {
+        int vertex;
+        int distance;
+
+        public NodeData(int vertex, int
+                distance) {
+            this.vertex = vertex;
+            this.distance = distance;
+        }
+
+        @Override
+        public int compareTo(NodeData o) {
+            return this.distance - o.distance;
+        }
+    }
+
+    public static void dijestrasAlgorithmTofindtheShortestPath(List<ArrayList<Edge>> graphDs, boolean[] visited, int[] distance, int src) {
+        PriorityQueue<NodeData> pq = new PriorityQueue<NodeData>();
+        for (int i = 0; i < graphDs.size(); i++) {
+            if (i != src) {
+                distance[i] = Integer.MAX_VALUE;
+            }
+        }
+            pq.add(new NodeData(src, 0));
+
+            while (!pq.isEmpty()) {
+                NodeData node = pq.remove();
+                if (!visited[node.vertex]) {
+                    visited[node.vertex]=true;
+                    graphDs.get(node.vertex).forEach(edge -> {
+                        int u = node.vertex;
+                        int v = edge.getDestination();
+                        int weight = edge.getWeight();
+
+                        if (distance[u] + weight < distance[v]) {
+                            distance[v] = distance[u] + weight;
+                            pq.add(new NodeData(edge.getDestination(),distance[v]));
+                        }
+
+                    });
+                }
+            }
+        for(int i=0;i<distance.length;i++) {
+            System.out.print(distance[i]+" ");
+        }
+        }
+
+
+
+
+
+
+    public static void dfsTest(List<ArrayList<Edge>> sampleGraph, Boolean[] visitedGraph, int currentVertex) {
+        if (visitedGraph[currentVertex]) {
+            return;
+        }
+        System.out.println(currentVertex);
+        visitedGraph[currentVertex] = true;
+        sampleGraph.get(currentVertex).forEach(v -> {
+            dfsTest(sampleGraph, visitedGraph, v.getDestination());
+        });
+
+    }
+
+    public static void bfsTest(List<ArrayList<Edge>> sampleGraph) {
+        boolean[] visitedGraph = new boolean[sampleGraph.size()];
+        Queue<Integer> graphQueue = new LinkedList<>();
+        graphQueue.add(0);
+        int vertex = 0;
+
+        while (!graphQueue.isEmpty()) {
+            if (!visitedGraph[graphQueue.peek()]) {
+                vertex = graphQueue.remove();
+                System.out.print(vertex + " ");
+                visitedGraph[vertex] = true;
+                sampleGraph.get(vertex).forEach(v -> {
+                    graphQueue.add(v.getDestination());
+                });
+            } else {
+                graphQueue.remove();
+            }
+
+        }
+
 
     }
 
 
     public void depthFirstSearch(List<ArrayList<Edge>> graphDS, boolean[] visited, int currentVertex) {
 
-        if (visited[currentVertex]==true) {
+        if (visited[currentVertex] == true) {
             return;
         }
         System.out.println(currentVertex);
@@ -102,21 +233,39 @@ public class GraphsDS {
         });
     }
 
-    public void printAllPathsUsingDFS(List<ArrayList<Edge>> graphDS,boolean[] visited,int currentVertex,int target,String path){
-        if(currentVertex==target){
+    public void printAllPathsUsingDFS(List<ArrayList<Edge>> graphDS, boolean[] visited, int currentVertex, int target, String path) {
+        if (currentVertex == target) {
             System.out.println(path);
             return;
         }
         graphDS.get(currentVertex).forEach(edge -> {
 
-            if(visited[currentVertex]==false) {
-                visited[currentVertex]=true;
-                printAllPathsUsingDFS(graphDS, visited, edge.getDestination(),target,path+"-->"+edge.getDestination());
-                visited[currentVertex]=false;
+            if (visited[currentVertex] == false) {
+                visited[currentVertex] = true;
+                printAllPathsUsingDFS(graphDS, visited, edge.getDestination(), target, path + "-->" + edge.getDestination());
+                visited[currentVertex] = false;
             }
         });
 
 
+    }
+
+    public static boolean cycleDetection(List<ArrayList<Edge>> graphDs, boolean[] visited, int current, int parent) {
+        visited[current] = true;
+
+        for (int i = 0; i < graphDs.get(current).size(); i++) {
+            Edge e = graphDs.get(current).get(i);
+            if (e.getDestination() != parent && visited[e.getDestination()]) {
+                System.out.println("cycle dectected..");
+                return true;
+            } else if (e.getDestination() != parent && !visited[e.getDestination()]) {
+                if (cycleDetection(graphDs, visited, e.getDestination(), current)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     public static void breadthFirstSearchAlgorithm(List<ArrayList<Edge>> graphDS, boolean[] visitedList, int start) {
